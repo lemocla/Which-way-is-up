c2f7b924be03d7f5751aaa0032dfccf375e7a37e
lemocla <lemocla@icloud.com>
2021-12-17 15:09:46 +0000

    Add contact form class,  layout & post request to views.py

---
+++ contact/forms.py
@@ -0,0 +1,40 @@
+"""
+form for contact forms
+"""
+
+from django import forms
+
+# https://docs.djangoproject.com/en/4.0/topics/forms/
+
+
+class ContactForm(forms.Form):
+    """
+    form
+    """
+    full_name = forms.CharField(
+        label='Full name',
+        required=True,
+        min_length=1, max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Your name'}),
+        error_messages={'required': 'Please enter your name'}
+    )
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=255,
+        widget=forms.TextInput(attrs={'placeholder': 'Your email address'}),
+        error_messages={'required': 'Please enter a valid email address'}
+    )
+    subject = forms.CharField(
+        label='Subject',
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Subject'}),
+        max_length=100
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': '6',
+                                     'placeholder': 'Your message'}),
+        required=True, min_length=3,
+        max_length=1500,
+        error_messages={'required': 'Please enter a message in the text area'}
+    )
--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,29 @@
 """
 Views to render contact form content
 """
+from django.contrib import messages
 from django.shortcuts import render
+from .forms import ContactForm
+
+# https://docs.djangoproject.com/en/4.0/topics/forms/
 
 
 def contact(request):
     """
-    View to return the contact us page
+    View to return the contact us page and create blank form
+    Post request to send email to shop owner
+    Display success message if the form is valid
     """
-    return render(request, 'contact/contact.html')
+    # Process the form data when post
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # display success message
+            messages.success(request, 'Your message has been sent '
+                             'successfully! Thanks for your email')
+            form = ContactForm()
+    # create a blank form
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
