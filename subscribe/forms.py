8dc53d3176505e4e8846ff7c4765602559308f81
lemocla <lemocla@icloud.com>
2021-12-19 15:34:21 +0000

    Create model for subscription and refactor form

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -3,21 +3,29 @@ form for newsletter subscription
 """
 
 from django import forms
-
+from .models import Subscription
 # https://docs.djangoproject.com/en/4.0/topics/forms/
 
 
-class SubscribeForm(forms.Form):
+class SubscribeForm(forms.ModelForm):
     """
     form fields for newsletter subscription
+    based on Subscription model
     """
+    class Meta:
+        model = Subscription
+        fields = '__all__'
 
-    email = forms.EmailField(
-        label='Email',
-        required=False,
-        max_length=255,
-        widget=forms.EmailInput(attrs={'placeholder': 'name@example.com'}),
-        error_messages={'required': 'Please enter a valid email address'}
-    )
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs["placeholder"] =  "name@example.com"
+        self.fields["email"].widget.attrs["class"] = "form-control"
+        self.fields["email"].error_messages["required"] = "Please enter a valid email address"
 
-    email.widget.attrs.update({'class': 'form-control'})
+
+
+ 
--- subscribe/models.py
+++ subscribe/models.py
@@ -1,3 +1,15 @@
+'''
+Models for newsletter subscription
+'''
 from django.db import models
 
+
 # Create your models here.
+class Subscription(models.Model):
+    '''
+    Model to register email address
+    '''
+    email = models.EmailField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.email)
