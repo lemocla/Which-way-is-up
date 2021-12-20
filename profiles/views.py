17cd4998f5ac3101e2756d0680923c58ab199672
lemocla <lemocla@icloud.com>
2021-12-20 00:55:06 +0000

    Add content, layout and styles to profile template

--- profiles/views.py
+++ profiles/views.py
@@ -4,10 +4,13 @@ Views to render my profile page content
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import DeliveryForm, ContactDetailsForm
 
 @login_required
 def profile(request):
     """
     View to return the profile page
     """
-    return render(request, 'profiles/profile.html')
+    del_form = DeliveryForm()
+    cont_form = ContactDetailsForm()
+    return render(request, 'profiles/profile.html', {'del_form': del_form, 'cont_form': cont_form})
