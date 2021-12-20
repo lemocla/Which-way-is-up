badb2e1caaa337542eabb5acc1066e583eb51513
lemocla <lemocla@icloud.com>
2021-12-20 00:54:04 +0000

    Add user profile model and form

---
+++ profiles/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import UserProfile
+
+class DeliveryForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user', 'full_name', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County, State or Locality',
+        }
+ 
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+
+class ContactDetailsForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user', 'street_address1', 'street_address2', 'postcode', 'county', 'town_or_city', 'country')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'phone_number': 'Phone Number',
+        }
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['placeholder'] = placeholders[field]
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class UserProfile(models.Model):
+    """
+    A user profile model for default delivery information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=80, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
