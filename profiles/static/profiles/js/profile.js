309ff51cf48e46cf09b808ccdec47886ec048e18
lemocla <lemocla@icloud.com>
2021-12-20 15:20:05 +0000

    Update profile template & styles for country and boolean fields

---
+++ profiles/static/profiles/js/profile.js
@@ -0,0 +1,14 @@
+
+// Adapted from Code Institute Boutique Ado project
+let countrySelected = $('#id_country').val();
+if(!countrySelected) {
+    $('#id_country').css('color', '#6c767d');
+};
+$('#id_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#6c767d');
+    } else {
+        $(this).css('color', '#0D0000');
+    }
+});
