e57d1d8386b16577bb999ab9c19d03462a072aa4
lemocla <lemocla@icloud.com>
2021-12-21 20:50:23 +0000

    Refactor categories process & base template dynamic menu

--- portfolio/context_categories.py
+++ portfolio/context_categories.py
@@ -2,17 +2,26 @@
 Context to display subscribe form to all pages
 """
 
-from .models import PortfolioCategory
+from .models import PortfolioCategory, Portfolio
 
 
 def categories(request):
     """
-    Put subscibe form in context
+    Build a dictionary to use for navigation in base.html
+    Put the dictionary in the context
     """
     categories_object = PortfolioCategory.objects.all()
+    portfolio_object = Portfolio.objects.all()
     cat = []
     for item in categories_object:
-        cat.append(item.name)
+        portfolio_object = Portfolio.objects.filter(category=item.id)
+        portfolio_list = []
+        # get portfolio for category item
+        for portfolio in portfolio_object:
+            portfolio_list.append(portfolio.name)
+        # build dictionary for navigation in base.html
+        dic = {'name': item.name, 'col': portfolio_list}
+        cat.append(dic)
 
     context = {'categories': cat}
     return context
