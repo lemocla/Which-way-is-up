cc79685e37be4b33cc24a2eddb33dcd9717ff80b
lemocla <lemocla@icloud.com>
2021-12-22 23:12:38 +0000

    Update homepage to render featured portfolio dynamically

--- home/views.py
+++ home/views.py
@@ -3,9 +3,16 @@ Views to render homepage content
 """
 from django.shortcuts import render
 
+from portfolio.models import Portfolio
+
 
 def index(request):
     """
     View to return the index page
     """
-    return render(request, 'home/index.html')
+    portfolio = Portfolio.objects.filter(
+                homepage=True)[0]
+    context = {
+        'portfolio': portfolio
+    }
+    return render(request, 'home/index.html', context)
