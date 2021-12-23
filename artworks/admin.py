099dc3e3deacaea03964ae5b81c06003ec003adc
lemocla <lemocla@icloud.com>
2021-12-23 12:06:01 +0000

    Add model and configure admin panel for shop categories

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+"""
+Admin models for portfolio app
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import ShopCategory
+
+
+class ShopCategoryAdmin(admin.ModelAdmin):
+    """
+    Admin model to display user portfolio categories
+    Display category name
+    """
+    model = ShopCategory
+    list_display = (
+        "name",
+    )
+    ordering = ('created_at',)
+
+
+admin.site.register(ShopCategory, ShopCategoryAdmin)
--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+"""
+Models to manage artwork and shop categories
+"""
 
-# Create your models here.
+from django.db import models
+from portfolio.fields import CaseInsensitiveCharField
+
+
+class ShopCategory(models.Model):
+    """
+    Model for shop categories
+    """
+    class Meta:
+        """Order by name"""
+        ordering = ['created_at']
+        verbose_name_plural = "Shop categories"
+
+    name = CaseInsensitiveCharField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
