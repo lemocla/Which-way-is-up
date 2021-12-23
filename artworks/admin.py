9f91bc0a4a6be3b0ea25620de8f95ffd6e1148c9
lemocla <lemocla@icloud.com>
2021-12-23 18:54:32 +0000

    Add artwork model and admin panel

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,9 +1,10 @@
 """
-Admin models for portfolio app
+Admin models for artwork app
 """
 
 from django.contrib import admin
-from .models import ShopCategory
+from django.utils.safestring import mark_safe
+from .models import ShopCategory, Artwork
 
 
 class ShopCategoryAdmin(admin.ModelAdmin):
@@ -19,4 +20,35 @@ class ShopCategoryAdmin(admin.ModelAdmin):
     ordering = ('created_at',)
 
 
+class ArtworkAdmin(admin.ModelAdmin):
+    """
+    Admin model to display user portfolio categories
+    Display category name
+    """
+
+    model = Artwork
+    # https://stackoverflow.com/questions/2443752/django-display-image-in-admin-interface
+
+    def admin_image(self, model):
+        """
+        Render image in admin panel
+        """
+        return mark_safe(
+            '<img src="/media/%s" width="50" height="50"/>' % model.image)
+    admin_image.allow_tags = True
+
+    list_display = (
+        "admin_image",
+        "name",
+        "size",
+        "price",
+        "stock",
+        "portfolio",
+        "shop_category",
+        "status"
+    )
+    ordering = ('name',)
+
+
 admin.site.register(ShopCategory, ShopCategoryAdmin)
+admin.site.register(Artwork, ArtworkAdmin)
--- artworks/models.py
+++ artworks/models.py
@@ -3,7 +3,9 @@ Models to manage artwork and shop categories
 """
 
 from django.db import models, transaction
+from django.core.validators import MaxValueValidator, MinValueValidator
 from portfolio.fields import CaseInsensitiveCharField
+from portfolio.models import Portfolio
 
 
 class ShopCategory(models.Model):
@@ -16,7 +18,9 @@ class ShopCategory(models.Model):
         verbose_name_plural = "Shop categories"
 
     name = CaseInsensitiveCharField(max_length=150, unique=True)
-    backend_name = CaseInsensitiveCharField(max_length=150, null=True, blank=True)
+    backend_name = CaseInsensitiveCharField(max_length=150,
+                                            null=True,
+                                            blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -27,3 +31,64 @@ class ShopCategory(models.Model):
             str_name = self.name
             self.backend_name = str_name.replace(' ', '_')
             return super(ShopCategory, self).save(*args, **kwargs)
+
+
+class Artwork(models.Model):
+    """
+    Model for portfolio
+    """
+    class Meta:
+        """Order by name"""
+        ordering = ['name']
+
+    ACTIVE = 'active'
+    DRAFT = 'draft'
+    INACTIVE = 'inactive'
+
+    STATUS = [
+        (ACTIVE, 'active'),
+        (DRAFT, 'draft'),
+        (INACTIVE, 'inactive'),
+    ]
+
+    name = CaseInsensitiveCharField(max_length=150, unique=True)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField(max_length=1500, null=True, blank=True)
+    materials = models.CharField(max_length=500)
+    size = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                validators=[MinValueValidator('0.00')])
+    # https://stackoverflow.com/questions/849142/how-to-limit-the-maximum-value-of-a-numeric-field-in-a-django-model
+    stock = models.PositiveSmallIntegerField(default=1,
+                                             validators=[
+                                                MaxValueValidator(500),
+                                                MinValueValidator(1)
+                                             ])
+    portfolio = models.ForeignKey(Portfolio,
+                                  null=True,
+                                  blank=True,
+                                  on_delete=models.SET_NULL)
+    shop_category = models.ForeignKey('ShopCategory',
+                                      null=True,
+                                      blank=True,
+                                      on_delete=models.SET_NULL)
+    related_items = models.ForeignKey("self", null=True,
+                                      blank=True, on_delete=models.SET_NULL)
+    on_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=6,
+                                     decimal_places=2,
+                                     null=True,
+                                     blank=True,
+                                     validators=[
+                                        MinValueValidator('0.00')
+                                     ])
+    rating = models.DecimalField(max_digits=4,
+                                 decimal_places=2,
+                                 null=True,
+                                 blank=True)
+    status = models.CharField(max_length=10, choices=STATUS, default=ACTIVE)
+    display_shop = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
