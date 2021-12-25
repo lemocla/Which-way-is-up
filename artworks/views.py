0e42baeae5114fe18c96eb065b65b3c131c516f3
lemocla <lemocla@icloud.com>
2021-12-25 19:45:47 +0000

    Add styles and content to artworks template and link in base template

--- artworks/views.py
+++ artworks/views.py
@@ -1,9 +1,9 @@
 """
 Views to manage artworks CRUD operations
 """
-
 from django.shortcuts import render
-from .models import ShopCategory
+
+from .models import ShopCategory, Artwork
 
 
 def artworks(request):
@@ -11,14 +11,21 @@ def artworks(request):
     View to return all artworks page
     """
     shop_category = None
+    items = Artwork.objects.filter(display_shop=True,
+                                   status="active")
 
     if request.GET:
         if 'shop_category' in request.GET:
             shop_category = request.GET['shop_category']
-            shop_category = ShopCategory.objects.filter(backend_name=shop_category)[0]
+            shop_category = ShopCategory.objects.filter(
+                            backend_name=shop_category)[0]
+            items = Artwork.objects.filter(shop_category=shop_category,
+                                           display_shop=True,
+                                           status="active")
 
     context = {
         'category': shop_category,
+        'artworks': items
     }
 
     return render(request, 'artworks/artworks.html', context)
