ef718b2a297618734d077fe868a8f18e8b8001ef
lemocla <lemocla@icloud.com>
2022-01-03 19:12:46 +0000

    Add feature to limit quantity to available stock & calc estimate delivery date

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,5 +1,5 @@
 from django import template
-
+from datetime import date, timedelta
 
 register = template.Library()
 
@@ -7,3 +7,9 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+
+# https://stackoverflow.com/questions/19598213/generating-a-date-relative-to-another-date-in-django-template
+@register.filter(name='calc_est_delivery')
+def plus_days(days):
+    return date.today() + timedelta(days=days)
