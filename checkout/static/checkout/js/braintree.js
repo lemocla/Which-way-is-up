8fb4649fcbd4f6611015b91d88801d88f924909f
lemocla <lemocla@icloud.com>
2022-01-05 10:17:00 +0000

    Move checkout scripts to static folder

---
+++ checkout/static/checkout/js/braintree.js
@@ -0,0 +1,67 @@
+// Braintee payment form creation and intregration
+var form = document.querySelector('#checkout-form');
+var submit = document.querySelector('#checkout-submit');
+// retrieving client_token from template
+const client_token = JSON.parse(document.getElementById('client_token').textContent);
+
+// Braintree client token
+braintree.client.create({
+    authorization: client_token
+}, function (clientErr, clientInstance) {
+    if (clientErr) {
+        console.error(clientErr);
+        return;
+    }
+
+    // Braintree hosted fields
+    braintree.hostedFields.create({
+        client: clientInstance,
+        styles: {
+            'input': {
+                'font-size': '1rem',
+                'border-color': 'black',
+            },
+            'input.invalid': {
+                'color': 'red'
+            },
+            'input.valid': {
+                'color': 'green'
+            }
+        },
+        fields: {
+            number: {
+                container: '#card-number',
+                placeholder: 'card number ex. 4111 1111 1111 1111',
+            },
+            cvv: {
+                container: '#cvv',
+                placeholder: 'cvv ex. 123'
+            },
+            expirationDate: {
+                container: '#expiration-date',
+                placeholder: 'exp. date ex. 10/2022'
+            }
+        }
+    }, function (hostedFieldsErr, hostedFieldsInstance) {
+        if (hostedFieldsErr) {
+            console.error(hostedFieldsErr);
+            return;
+        }
+
+        submit.removeAttribute('disabled');
+
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
+                if (tokenizeErr) {
+                    console.error(tokenizeErr);
+                    return;
+                }
+                // Paiement submission
+                document.querySelector('input[name="payment_method_nonce"]').value = payload.nonce;
+                document.getElementById('checkout-form').submit();
+            });
+        }, false);
+    });
+});
---
+++ checkout/static/checkout/js/form.js
@@ -0,0 +1,75 @@
+// on load - display gift option if option is checked
+if ($('#is_auth').val() == 'true') {
+    $('#id_email').prop('readonly', 'readonly');
+}
+
+if ($('#is_gift').val() == 'on') {
+    $('#id_gift_option').attr('checked', true)
+    $('#gift-option-container').removeClass("hide");
+    if ($('#gift-message').val() != "None") {
+        let msg = $('#gift-message').val();
+        $('#id_gift_message').val(msg)
+    }
+}
+
+// default display for billing address block
+if ($('#id_billing_same_as_delivery').is(":checked")) {
+    $('#billing-details-container').addClass('hide');
+} else {
+    $('#billing-details-container').removeClass('hide');
+}
+
+// remove default required attribute
+$('#id_billing_street_address1').prop('required', false);
+$('#id_billing_town_or_city').prop('required', false);
+$('#id_billing_postcode').prop('required', false);
+$('#id_billing_country').prop('required', false);
+
+// toggle billing container if delivery details are different than billing details 
+$('#id_billing_same_as_delivery').change(function () {
+    console.log('billing option is change')
+    if ($('#id_billing_same_as_delivery').is(":checked")) {
+        $('#billing-details-container').addClass('hide');
+        $('#id_billing_street_address1').val($('#id_delivery_street_address1').val()).prop('required', false);
+        $('#id_billing_street_address2').val($('#id_delivery_street_address2').val());
+        $('#id_billing_town_or_city').val($('#id_delivery_town_or_city').val()).prop('required', false);
+        $('#id_billing_postcode').val($('#id_delivery_postcode').val()).prop('required', false);
+        $('#id_billing_county').val($('#id_delivery_county').val());
+        $('#id_billing_country').val($('#id_delivery_country').val()).prop('required', false);
+    } else {
+        $('#billing-details-container').removeClass('hide');
+        $('#id_billing_street_address1').val("").prop('required', true);
+        $('#id_billing_street_address2').val("");
+        $('#id_billing_town_or_city').val("").prop('required', true);
+        $('#id_billing_postcode').val("").prop('required', true);
+        $('#id_billing_county').val("");
+        $('#id_billing_country').val("").prop('required', true);
+    }
+})
+
+// display gift message container
+$('#gift-checkbox').change(function () {
+    if ($('#gift-checkbox').is(":checked")) {
+        $('#form-message-container').removeClass('hide');
+    } else {
+        $('#form-message-container').addClass('hide');
+    }
+})
+
+// add gift option
+$('#id_gift_option').change(function () {
+    if ($('#id_gift_option').is(":checked")) {
+        // display gift option container
+        $('#gift-option-container').removeClass("hide");
+        // set is gift to true
+        $('#is_gift').prop('checked', true);
+    } else {
+        $('#gift-option-container').addClass("hide");
+        $('#form-message-container').addClass('hide');
+        // remove values of field 
+        $('#is_gift').prop('checked', false);
+        $('#gift-checkbox').prop('checked', false);
+        $('#id_gift_recipient').val("");
+        $('#id_gift_message').val("");
+    }
+})
