5ca6aec1cbc06e5340f104d5c6bb2ecc10b3536f
lemocla <lemocla@icloud.com>
2022-01-08 21:56:28 +0000

    Add model and form for events in about app

---
+++ about/forms.py
@@ -0,0 +1,40 @@
+"""
+Form definition for add and edit an event
+"""
+from django import forms
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+    """
+    Model form to add/edit an portfolio
+    """
+    class Meta:
+        """All fields from event model """
+        model = Event
+        fields = '__all__'
+
+    description = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={
+                              'rows': '3',
+                              'placeholder': 'Enter event desciption'}))
+    # https://stackoverflow.com/questions/61076688/django-form-dateinput-with-widget-in-update-loosing-the-initial-value
+    date_start = forms.DateField(
+        widget=forms.DateInput(
+            format=['%d-%m-%Y'],
+            attrs={'placeholder': 'Enter date ex. 10/01/2021',
+                   'type': 'date'}))
+    date_end = forms.DateField(
+        required=False,
+        widget=forms.DateInput(
+            format=('%d-%m-%Y'),
+            attrs={'placeholder': 'Enter date ex. 10/01/2021',
+                   'type': 'date'}))
+
+    place = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Enter event name & location'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- about/models.py
+++ about/models.py
@@ -1,3 +1,24 @@
+"""
+Models for about app
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    Model to store event details
+    """
+    class Meta:
+        """Order by name"""
+        ordering = ['-date_start']
+
+    date_start = models.DateField(auto_now=False, auto_now_add=False,
+                                  null=False, blank=False)
+    date_end = models.DateField(auto_now=False, auto_now_add=False, null=True,
+                                blank=True)
+    place = models.CharField(max_length=500, blank=False, null=False)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'{self.date_start} - {self.place}'
