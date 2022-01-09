fef686cde3d597d57f8112256e1fecb46880893c
lemocla <lemocla@icloud.com>
2022-01-09 23:46:34 +0000

    Add frontend validation on paiement section checkout form

--- checkout/static/checkout/js/braintree.js
+++ checkout/static/checkout/js/braintree.js
@@ -45,10 +45,74 @@ braintree.client.create({
         if (hostedFieldsErr) {
             return;
         }
+        // Braintree validation for hosted fields from
+        // https://codepen.io/braintree/pen/zeamxM
+ 
+        function createInputChangeEventListener(element) {
+            return function () {
+                validateInput(element);
+            }
+        }
+
+        function setValidityClasses(element, validity) {
+            if (validity) {
+                element.removeClass('is-invalid');
+                element.addClass('is-valid');
+            } else {
+                element.addClass('is-invalid');
+                element.removeClass('is-valid');
+            }
+        }
+
+        function validateInput(element) {
+            // validation - if fields are empty, mark them
+            // as invalid, if not, mark them as valid
+            if (!element.val().trim()) {
+                setValidityClasses(element, false);
+                return false;
+            }
+            setValidityClasses(element, true);
+            return true;
+        }
+
+        hostedFieldsInstance.on('validityChange', function (event) {
+            var field = event.fields[event.emittedBy];
+
+            // Remove any previously applied error or warning classes
+            $(field.container).removeClass('is-valid');
+            $(field.container).removeClass('is-invalid');
+
+            if (field.isValid) {
+                $(field.container).addClass('is-valid');
+            } else if (field.isPotentiallyValid) {
+                // skip adding classes if the field is
+                // not valid, but is potentially valid
+            } else {
+                $(field.container).addClass('is-invalid');
+            }
+        });
+
         // Form submission
         submit.removeAttribute('disabled');
         form.addEventListener('submit', function (event) {
             event.preventDefault();
+            var formIsInvalid = false;
+            var state = hostedFieldsInstance.getState();
+            // Loop through the Hosted Fields and check
+            // for validity, apply the is-invalid class
+            // to the field container if invalid
+            Object.keys(state.fields).forEach(function (field) {
+                if (!state.fields[field].isValid) {
+                    $(state.fields[field].container).addClass('is-invalid');
+                    formIsInvalid = true;
+                }
+            });
+
+            if (formIsInvalid) {
+                // skip tokenization request if any fields are invalid
+                return;
+            }
+
             hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
                 if (tokenizeErr) {
                     return;
