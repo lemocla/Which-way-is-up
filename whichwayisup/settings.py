60c5970608eca15fa3590ce2d8fb99bb6aae872e
Claire Lemonnier <claireci.lemonnier@gmail.com>
2023-05-06 16:40:40 +0100

    Add vercel.json and update settings

--- whichwayisup/settings.py
+++ whichwayisup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '.vercel.app']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
