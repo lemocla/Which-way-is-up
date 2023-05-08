5d176700833b33d7c60d524e8bb2c40fe671b726
Claire Lemonnier <claireci.lemonnier@gmail.com>
2023-05-08 20:21:05 +0100

    Clean repo and remove vercel settings

--- whichwayisup/settings.py
+++ whichwayisup/settings.py
@@ -27,11 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
