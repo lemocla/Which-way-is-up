0aaf4384cb587e52c96be6364242a4d5f094d7ca
Claire Lemonnier <claireci.lemonnier@gmail.com>
2023-05-08 22:17:26 +0100

    Update settings host env variable

--- whichwayisup/settings.py
+++ whichwayisup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('HOST')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
