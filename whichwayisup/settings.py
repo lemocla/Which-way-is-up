6e360a5e94ecb8a1573837c32dd2d656f3a545d9
Claire Lemonnier <claireci.lemonnier@gmail.com>
2023-05-08 21:54:08 +0100

    add procfile & update allowed hosts

--- whichwayisup/settings.py
+++ whichwayisup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('HOST')]
 
 # Application definition
 
